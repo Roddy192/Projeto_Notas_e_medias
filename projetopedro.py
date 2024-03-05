2c3a66dbf4b070dfce7e7bd7792555ea44bfd3ab
Roddy192 <rodrigo.bgarcia@hotmail.com>
2024-03-05 13:17:49 -0300

    Proj Pedro

---
+++ projetopedro.py
@@ -0,0 +1,20 @@
+#entrada de dados
+
+notas = {Joao : [], Maria : [], Lucas : [], Ana : []}
+medias = {Joao : [], Maria : [], Lucas : [], Ana : []}
+
+#Apresentação do programa ao usuário
+
+print("Bem vindo ao programa de Notas e Médias! /n")
+
+while opc
+opc=(input("Escolha uma opção: Listar alunos (1) Pesquisar notas por aluno (2) Pesquisar média por aluno (3) Sair(4)"))
+
+
+
+
+
+
+
+
+
