69749218982a24cfced51bfdf2c55b63be4b67c3
Roddy192 <rodrigo.bgarcia@hotmail.com>
2024-03-05 13:58:51 -0300

    Proj Pedro

--- projetopedro.py
+++ projetopedro.py
@@ -1,16 +1,19 @@
-#entrada de dados
-
-notas = {Joao : [], Maria : [], Lucas : [], Ana : []}
-medias = {Joao : [], Maria : [], Lucas : [], Ana : []}
-
 #Apresentação do programa ao usuário
 
 print("Bem vindo ao programa de Notas e Médias! /n")
 
-while opc
-opc=(input("Escolha uma opção: Listar alunos (1) Pesquisar notas por aluno (2) Pesquisar média por aluno (3) Sair(4)"))
+def inicio():
+    escolha=(int(input("Escolha uma opção: Listar alunos (1) Pesquisar notas por aluno (2) Pesquisar média por aluno (3) Sair(4) Retornar ao menu (5)")))
+    if escolha > 5:
+        print("Essa opção é inválida. Escolha novamente!")
+        inicio()
+
+    elif escolha == 4:
+        #fechar programa
+        print("O programa será fechado!")
 
 
+inicio()
 
 
 
